c52f086b01c7f6940fbadd780919cda674995a30
ChiselD <chisel.delahanty@gmail.com>
2017-06-29 23:42:16 -0400

    add painting function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-// let numSquares = parseInt(prompt("Select number of squares per side"));
+let side = 16; // number of squares per side
+// ...parseInt(prompt("Select number of squares per side"));
 
 $(document).ready(function(){
-	for (let i = 0; i < 4; i++) {
+	for (let i = 0; i < (side * side); i++) {
 		$("#grid").append("<div class='square'></div>");
 	}
-	$(".square").css("width", 900/4);
-	$(".square").css("height", 900/4);
+	$(".square").css("width", 900/side);
+	$(".square").css("height", 900/side);
+});
+
+$(document).ready(function(){
+	$(".square").mouseenter(function(){
+		$(this).addClass("colorify");
+	});
 });
