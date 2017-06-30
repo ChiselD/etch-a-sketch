479fa8be209391f9e45455362cf725f95e1b3a33
ChiselD <chisel.delahanty@gmail.com>
2017-06-29 23:25:10 -0400

    create square-generating function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("connected");
+// let numSquares = parseInt(prompt("Select number of squares per side"));
+
+$(document).ready(function(){
+	for (let i = 0; i < 4; i++) {
+		$("#grid").append("<div class='square'></div>");
+	}
+	$(".square").css("width", 900/4);
+	$(".square").css("height", 900/4);
+});
