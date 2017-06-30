49b93d3a984fff61e69fe4e3edee94e9c2a5b0b5
ChiselD <chisel.delahanty@gmail.com>
2017-06-30 00:02:41 -0400

    create Clean Slate function; separate out other functions

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 let side = 16; // number of squares per side
-// ...parseInt(prompt("Select number of squares per side"));
 
-$(document).ready(function(){
+function createGrid() {
 	for (let i = 0; i < (side * side); i++) {
 		$("#grid").append("<div class='square'></div>");
 	}
 	$(".square").css("width", 900/side);
 	$(".square").css("height", 900/side);
-});
+}
 
-$(document).ready(function(){
+function paint() {
 	$(".square").mouseenter(function(){
 		$(this).addClass("colorify");
 	});
-});
+}
+
+$(document).ready(createGrid());
+$(document).ready(paint());
+
+$("button").on("click", function(){
+	$(".square").removeClass("colorify");
+	side = parseInt(prompt("Select number of squares per side"));
+	createGrid();
+	paint();
+})
