2920f6062f1375cd8982c43718a161a5a16a8840
ChiselD <chisel.delahanty@gmail.com>
2017-07-08 23:09:02 -0400

    change createGrid function to use columns; update Clear Slate function accordingly

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 let side = 16; // number of squares per side
 
-// Note to self: this might help for the 'keep-it-square' branch:
-// https://stackoverflow.com/questions/15004537/css-div-alternating-colour
-
 function createGrid() {
-	for (let i = 0; i < (side * side); i++) {
-		$("#grid").append("<div class='square' id='" + i + "'></div>");
+	for (let i = 0; i < side; i++) {
+		$("#grid").append("<div class='column'></div>");
 	}
+	for (let i = 0; i < side; i++) {
+		$(".column").append("<div class='square'></div>");
+	}
+	$(".column").css("width", 800/side + "px");
+	$(".column").css("height", 800 + "px");
 	$(".square").css("width", 800/side + "px");
 	$(".square").css("height", 800/side + "px");
 }
@@ -36,6 +38,7 @@ $(document).ready(paint());
 // "Clear Slate" function that refreshes the canvas and gets a new number of squares
 $("button").on("click", function(){
 	$(".square").removeClass("colorify");
+	$("#grid").empty();
 	getNumber();
 	createGrid();
 	paint();
